d31a267217648d3da869e3afd715600e4b5edad4
Dmitry <dmitry@smilecdr.com>
2026-02-08 00:25:45 -0700

    added test logging in user controller

--- src/main/java/com/ecommerce/user/controller/UserController.java
+++ src/main/java/com/ecommerce/user/controller/UserController.java
@@ -2,7 +2,11 @@ package com.ecommerce.user.controller;
 
 import com.ecommerce.user.dto.UserRequest;
 import com.ecommerce.user.dto.UserResponse;
+import com.ecommerce.user.model.User;
 import com.ecommerce.user.service.UserService;
+import lombok.extern.slf4j.Slf4j;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -11,8 +15,10 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/user")
+@Slf4j
 public class UserController {
     private UserService userService;
+    private static Logger logger = LoggerFactory.getLogger(UserController.class);
 
     public UserController(UserService userService) {
         this.userService = userService;
@@ -25,6 +31,8 @@ public class UserController {
 
     @GetMapping("/{id}")
     public ResponseEntity<UserResponse> getUser(@PathVariable String id) {
+        logger.info("Request received for user: {}", id);
+        logger.trace("This is a TRACE level - very detailed log");
         return userService.fetchUser(id)
                 .map(ResponseEntity::ok)
                 .orElseGet(() -> ResponseEntity.notFound().build());
